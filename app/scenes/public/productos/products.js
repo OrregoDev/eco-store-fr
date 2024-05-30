7c9da02f968ec904e222bf79db11b80b51a4556f
18zuluaga <18zuluaga1989@gmail.com>
2024-05-29 22:38:27 -0500

    Add modal product

--- app/scenes/public/productos/products.js
+++ app/scenes/public/productos/products.js
@@ -16,20 +16,46 @@ export function productsPage(){
 
         <a href="#/Productos_Para_el_Hogar" id="hogar" class="${style.botoncategorias}">Productos Para el hogar</a>
     </section>
-    <section class="${style.products}" id="products">
+    <section class="${style.products}" id="lista_2">
 
+    <div class="${style.padre}" id="modal">
+    <div class="${style.container}" id="lista_1">
+      <div class="${style.izquierda}">
+        <div class="${style.imagenes}" id="img_1">
+          <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+          <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+          <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+          <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="" />
+        </div>
+      </div>
+      <div class="${style.derecha}">
+        <div class="${style.textos}">
+          <h2 id="titulo_modal">Cepillo de dientes de bamboo</h2>
+          <div class="${style.colores}">
+            <p>Precio:</p>
+            <div class="${style.coloresdots}">
+              <div>$</div>
+              <div id="precio_modal"></div>
+              <div></div>
+            </div>
+          </div>
+          <a class="agregar_carrito">Add to cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
     </section>
 </section>
     `;
     const logic = () => {
         const card = (data) => {
-            console.log(data)
             let card = document.createElement('div')
             card.setAttribute('class', style.tarjeta)
             card.innerHTML = `
                             <img src=${data.image} alt="" class="imagen">
                             <h2 class="nombre">${data.name}</h2>
-                            <button id=${data.id} class="Comprar">Comprar</button>
+                            <p class="${style.precio} precio">${data.price}</p>
+                            <a id=${data.id} class="leer_mas">Leer mas</a>
                         `
             return card;
         }
@@ -53,9 +79,30 @@ export function productsPage(){
         const url = `http://localhost:4000/api/auth/get-products`;
         
         ajax(url,(data)=>{
-            console.log(data)
-            document.querySelector('#products').append(contenedortarjet(data))
+            document.querySelector('#lista_2').append(contenedortarjet(data))
         })
+        const elemetos1 = document.getElementById("lista_2");
+        elemetos1.addEventListener("click", Elemento);
+        
+        const modal_product = document.getElementById('modal');
+
+        function Elemento(e) {
+            console.log(e)
+            if (e.target.classList.contains("leer_mas")) {
+              const elemento = e.target.parentElement;
+              console.log(elemento,"aqui");
+              
+              let imagen = elemento.querySelector(".imagen").src
+              let titulo = elemento.querySelector("h2").textContent
+              let precio = elemento.querySelector(".precio").textContent
+              let id = elemento.querySelector("a").getAttribute("data-id")
+              console.log(imagen,titulo,precio,id)
+              document.getElementById('titulo_modal').textContent = titulo
+              modal_product.querySelector('img').src = imagen
+              document.getElementById('precio_modal').textContent = precio
+              modal_product.style.display = "flex"
+            }
+          }
       };
 
     return {
